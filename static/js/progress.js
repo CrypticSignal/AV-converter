e6bba671401fdb750561df333fb06669ce504c02
BassThatHertz <hshafiq@hotmail.co.uk>
2020-05-27 12:07:50 +0000

    Show encoder progress in the console.

--- static/js/progress.js
+++ static/js/progress.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     //connect to the socket server.
     const socket = io.connect('https://' + document.domain + ':' + location.port);
     // this is a callback that triggers when the "show progress" event is emitted by the server.
-    socket.on('show progress', function(msg) {   
+    socket.on('show progress', function(msg) {
         document.getElementById("progress").innerHTML = msg.progress;
+        console.log(msg.progress);
     });
 });
