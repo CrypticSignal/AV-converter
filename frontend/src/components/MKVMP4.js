4cb996a105fe4f8e74871d538d4341bfcc92b483
BassThatHertz <hshafiq@hotmail.co.uk>
2021-03-17 00:40:48 +0000

    Misc.

--- frontend/src/components/MKVMP4.js
+++ frontend/src/components/MKVMP4.js
@@ -1,28 +1,30 @@
 function MKVMP4(props) {
     return (
         <div id="video">
-            <label htmlFor="video_mode">FFmpeg setting:</label>
             <select id="video_mode" onChange={props.onVideoSettingChange} value={props.videoSetting}>
                 <option value="keep_codecs">[1] -c copy</option>
-                <option value="keep_video_codec">[2] -c:v copy -c:a libfdk_aac -vbr 5</option>
+                <option value="keep_video_codec">[2] -c:v copy -c:a libfdk_aac</option>
                 <option value="convert_video_keep_audio">[3] -c:v libx264 -c:a copy</option>
-                <option disabled>Or choose a preset:</option>
-                <option value="ultrafast">[4] Ultra fast</option>
-                <option value="superfast">[5] Super fast</option>
-                <option value="veryfast">[6] Very fast</option>
-                <option value="faster">[7] Faster</option>
-                <option value="fast">[9] Fast</option>
-                <option value="medium">[9] Medium</option>
-                <option value="slow">[10] Slow</option>
-                <option value="slower">[11] Slower</option>
-                <option value="veryslow">[12] Very slow</option>
+                <option disabled>Or choose an x264 preset:</option>
+                <option value="ultrafast">[4] -preset ultrafast</option>
+                <option value="superfast">[5] -preset superfast</option>
+                <option value="veryfast">[6] -preset veryfast</option>
+                <option value="faster">[7] -preset faster</option>
+                <option value="fast">[9] -preset fast</option>
+                <option value="medium">[9] -preset medium</option>
+                <option value="slow">[10] -preset slow</option>
+                <option value="slower">[11] -preset slower</option>
+                <option value="veryslow">[12] -preset veryslow</option>
             </select><br/>
-            <strong>When using a preset (options 4 to 12), the video codec is converted to H.264 (AVC) and the
-                audio codec is converted to AAC, using libfdk_aac with VBR mode 5. More details about the presets can be found
-                <a href="https://trac.ffmpeg.org/wiki/Encode/H.264#crf" target="_blank">here</a>.</strong><br /><br />
-            {/*CRF*/}
-            <div id="crf_div">
-                <p>Constant Rate Factor (CRF):</p>
+            
+            <i style={{ display: props.videoSetting.includes("keep") ? "none" : "block" }}>
+                When using a preset (options 4 to 12), the video codec is converted to H.264 (AVC) and the
+                audio codec is converted to AAC, using libfdk_aac with VBR mode 5. More details about the presets can be found 
+                <a href="https://trac.ffmpeg.org/wiki/Encode/H.264" target="_blank"> here</a>.
+            </i><br/>
+
+            <div id="crf_div" style={{ display: props.videoSetting.includes("keep") ? "none" : "block" }}>
+                <strong>Constant Rate Factor (CRF)</strong><br/>
                 <input type="range" className="slider" onChange={props.onCrfChange}
                     min={0} max={51} step={1} value={props.crfValue}  />
                 <span id="crf_value"/>{` ${props.crfValue}`}<br/>
