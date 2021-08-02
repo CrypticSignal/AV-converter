08141d50f244a2c4255e63915cc204267d330340
BassThatHertz <hshafiq@hotmail.co.uk>
2021-08-02 03:11:33 +0100

    Remove a file that is no longer used.

--- frontend/src/components/ProgressBar.js
+++
@@ -1,7 +0,0 @@
-import ProgressBar from "react-bootstrap/ProgressBar";
-
-function ProgressBarComponent(props) {
-  return <ProgressBar now={props.progress} label={`${props.progress}%`} />;
-}
-
-export default ProgressBarComponent;
