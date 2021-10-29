f08f45de20bd9fb82d19981ca4abdb89396a9962
BassThatHertz <hshafiq@hotmail.co.uk>
2021-10-29 13:27:57 +0100

    Put the logger in its own file.

---
+++ backend/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+from flask_app import ENVIRONMENT
+
+logging.basicConfig(
+    format="%(message)s",
+    level=10,
+    filename=os.path.join("logs", "backend.log"),
+)
+
+if ENVIRONMENT != "production":
+    # Add a stream handler to the root logger so that the messages are shown in the terminal too.
+    logging.getLogger().addHandler(logging.StreamHandler())
+
+log = logging.getLogger()
