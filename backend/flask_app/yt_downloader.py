23bfd3532d40b1c24665f2d285b6324cab8d4b90
BassThatHertz <hshafiq@hotmail.co.uk>
2022-01-03 20:16:00 +0000

    Misc.

--- backend/flask_app/yt_downloader.py
+++ backend/flask_app/yt_downloader.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-import json
-import os
 from pathlib import Path
 from time import time
 
@@ -9,7 +7,7 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from yt_dlp import YoutubeDL
 
-from flask_app.utils import return_download_path, write_to_file
+from flask_app.utils import delete_file, return_download_path, write_to_file
 from logger import log
 
 # The directory is relative to the location of run.py
@@ -18,23 +16,37 @@ download_dir = "../"
 
 def progress_hooks(data):
     if data["status"] == "downloading":
-        downloaded_megabytes = round(data["downloaded_bytes"] / 1_000_000, 1)
+        tmp_filename = Path(data["tmpfilename"]).name
+        tmp_filename_string = f"<br><i>Currently writing to <strong>{tmp_filename}<strong></i>"
+    
+        downloaded_mb = round(data["downloaded_bytes"] / 1_000_000, 1)
+        log.info(f"Downloaded: {downloaded_mb}")
 
-        if "total_bytes" in data:
-            total_megabytes = round(data['total_bytes'] / 1_000_000, 1)
-        elif "total_bytes_estimate" in data:
-            total_megabytes = round(data['total_bytes_estimate'] / 1_000_000, 1)
-            
-        percentage_progress = round((downloaded_megabytes / total_megabytes) * 100, 1)
-        speed = f"{round(data['speed'] / 1000)}kb/s" if data["speed"] else "unknown"
+        if "total_bytes" in data and data["total_bytes"]:
+            total_mb = round(data["total_bytes"] / 1_000_000, 1)
+        elif "total_bytes_estimate" in data and data["total_bytes_estimate"]:
+            total_mb = round(data["total_bytes_estimate"] / 1_000_000, 1)
+        else:
+            total_mb = None
+
+        if total_mb:
+            log.info(f"Total: {total_mb}")
+            percentage_progress = round((downloaded_mb / total_mb) * 100, 1)
+            progress_string = f"{percentage_progress}% of ~{total_mb} MB downloaded"
+        else:
+            progress_string = f"{downloaded_mb} MB downloaded"
+
+        speed = f"{round(data['speed'] / 1000)} kb/s" if data["speed"] else "unknown"
         eta = data["eta"] if data["eta"] else "unknown"
-        
-        if eta != "unknown": 
+
+        if eta != "unknown":
             minutes, seconds = divmod(eta, 60)
             eta = f"{minutes:02}:{seconds:02}"
 
-        progress_string = f"{percentage_progress}% of {total_megabytes}MB @ {speed} [ETA: {eta}]"
-        write_to_file(session["yt_progress_url"], progress_string)
+        write_to_file(
+            session["yt_progress_url"],
+            f"{progress_string} @ {speed} [ETA: {eta}]{tmp_filename_string}",
+        )
 
     elif data["status"] == "finished":
         write_to_file(session["yt_progress_url"], "Postprocessing...")
@@ -51,7 +63,7 @@ def run_youtube_dl(video_link, options):
             log.info(f'Progress File: {session["yt_progress_url"]}')
             return str(e), 500
         else:
-            os.remove(session["yt_progress_url"])
+            delete_file(session["yt_progress_url"])
             return "success"
 
 
