55f9709d60391adabe6c77a50ec089468b872dd5
BassThatHertz <hshafiq@hotmail.co.uk>
2023-02-18 21:05:45 +0000

    Use import/export syntax + misc.

--- backend/src/handleDownloadEvents.ts
+++ backend/src/handleDownloadEvents.ts
@@ -1,34 +1,38 @@
-const fs = require("fs");
+import { ChildProcess } from "child_process";
+import { createWriteStream, readdirSync } from "fs";
+import { Logger } from "./logger";
+import { Response } from "express";
 import { sendFile, deleteFile, purgeUnwantedFiles } from "./utils";
-const { Logger } = require("./logger");
 
 const log = new Logger();
 
-exports.handleDownloadEvents = (
+export function handleDownloadEvents(
   res: Response,
-  process: any,
+  process: ChildProcess,
   progressFilename: string,
   filenameWithoutExt: string
-) => {
-  process.stdout.on("data", (data: Buffer) => {
-    fs.createWriteStream(progressFilename).write(data);
+) {
+  process.stdout?.on("data", (data: Buffer) => {
+    if (data.toString() !== "\n") {
+      createWriteStream(progressFilename).write(data.toString().trim());
+    }
   });
 
-  process.stderr.on("data", (data: Buffer) => {
-    fs.createWriteStream(progressFilename).write(data);
-    log.error(data);
+  process.stderr?.on("data", (data: Buffer) => {
+    createWriteStream(progressFilename).write(data.toString().trim());
+    log.error(data.toString().trim());
   });
 
   process.on("close", (code: number) => {
-    if (code == 0) {
-      const filename = fs
-        .readdirSync(__dirname + "/../")
-        .filter((file: string) => file.includes(filenameWithoutExt))[0];
+    if (code === 0) {
+      const filename = readdirSync(__dirname + "/../").filter((file: string) =>
+        file.includes(filenameWithoutExt)
+      )[0];
 
       log.info(filename);
       sendFile(res, filename);
     }
     deleteFile(progressFilename);
-    purgeUnwantedFiles()
+    purgeUnwantedFiles();
   });
-};
+}
